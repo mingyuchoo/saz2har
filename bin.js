61ab8b7cfb5ccbec5a67f6af24530d1892de4062
mingyuchoo <mingyuchoo@gmail.com>
2020-08-14 09:05:36 +0900

    update modules and formatted code

--- bin.js
+++ bin.js
@@ -1,42 +1,47 @@
 #!/usr/bin/env node
 
-const path = require("path");
+const path = require('path');
+const argv = require('yargs')
+  .usage(
+    `Converts SAZ file (from Fiddler) to HAR file.
 
-const argv = require("yargs")
-    .usage(`Converts SAZ file (from Fiddler) to HAR file.
+Usage: $0 [options] input.saz [output.har]`
+  )
+  .option('no-validate', {
+    type: 'boolean',
+    description: 'Validate the output HAR file (default: true)',
+  })
+  .example('$0 foo.saz bar.har --no-validate').argv;
 
-Usage: $0 [options] input.saz [output.har]`)
-    .option("no-validate", {
-        type: "boolean",
-        description: "Validate the output HAR file (default: true)"
-    })
-    .example("$0 foo.saz bar.har --no-validate")
-    .argv;
-
-const saz2har = require("./");
+const saz2har = require('./');
 
 const sourceFile = argv.source || argv.s || argv._[0];
 const destinationFile = argv.destination || argv.d || argv._[1];
 
 const options = {
-    write: true,
-    validate: argv.validate
+  write: true,
+  validate: argv.validate,
 };
 
 const callback = (err, data) => {
-    if (err) {
-        console.error("Error: ", err);
+  if (err) {
+    console.error('Error: ', err);
 
-        return;
-    }
+    return;
+  }
 
-    const { source, destination } = data;
+  const { source, destination } = data;
 
-    console.log(`Successful conversion from ${source} to ${destination}`);
+  console.log(`Successful conversion from ${source} to ${destination}`);
 };
 
 if (sourceFile && destinationFile) {
-    saz2har.convert(path.resolve(sourceFile), path.resolve(destinationFile), options, callback);
+  saz2har.convert(
+    path.resolve(sourceFile),
+    path.resolve(destinationFile),
+    options,
+    callback
+  );
 } else if (sourceFile) {
-   saz2har.convert(path.resolve(sourceFile), options, callback);
+  saz2har.convert(path.resolve(sourceFile), options, callback);
 }
